9bd3fa85a9d4c8637da497a7a88c5cd39fc23ad9
小董 <liuhuakawaii@163.com>
2022-07-25 15:26:48 +0800

    kanbanniang

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -97,5 +97,91 @@ module.exports = {
   },
   "markdown": {
     "lineNumbers": true
-  }
+  },
+  plugins: [
+    //看板娘的配置
+    [
+      '@vuepress-reco/vuepress-plugin-kan-ban-niang',{
+        theme: [
+          'miku', 'whiteCat', 'haru1', 'haru2', 'haruto', 'koharu', 'izumi', 'shizuku', 'wanko', 'blackCat', 'z16'
+        ],
+        clean: false,
+        messages: { 
+          welcome: '欢迎来到我的博客', home: '心里的花，我想要带你回家。', theme: '好吧，希望你能喜欢我的其他小伙伴。', close: '你不喜欢我了吗？痴痴地望着你。' 
+        },
+        messageStyle: { right: '68px', bottom: '290px' },
+        width: 250,
+        height: 320
+      }
+    ],
+    //公告的配置
+    ['@vuepress-reco/vuepress-plugin-bulletin-popover', {
+      title: '公告',
+      body: [
+        {
+          type: 'title',
+          content: '欢迎来到我的主页 🎉🎉🎉',
+          style: 'text-aligin: center;',
+        },
+        {
+          type: 'text',
+          content: '喜欢的主题特效可以去个人信息',
+          style: 'text-align: center;'
+        },
+        {
+          type: 'text',
+          content: '友链或疑问均可在留言板给我留言',
+          style: 'text-align: center;'
+        }
+      ],
+      footer: [
+        {
+          type: 'button',
+          text: '打赏',
+          link: '/blog/donate'
+        },
+      ]
+    }],
+    //鼠标点击特效
+    [
+      "vuepress-plugin-cursor-effects",
+      {
+        size: 2,                    // size of the particle, default: 2
+        shape: 'circle',  // shape of the particle, default: 'star'
+        zIndex: 999999999           // z-index property of the canvas, default: 999999999
+      }
+    ],
+    //彩带特效
+    ["ribbon-animation", {
+      size: 90,   // 默认数据
+      opacity: 0.3,  //  透明度
+      zIndex: -1,   //  层级
+      opt: {
+        // 色带HSL饱和度
+        colorSaturation: "80%",
+        // 色带HSL亮度量
+        colorBrightness: "60%",
+        // 带状颜色不透明度
+        colorAlpha: 0.65,
+        // 在HSL颜色空间中循环显示颜色的速度有多快
+        colorCycleSpeed: 6,
+        // 从哪一侧开始Y轴 (top|min, middle|center, bottom|max, random)
+        verticalPosition: "center",
+        // 到达屏幕另一侧的速度有多快
+        horizontalSpeed: 200,
+        // 在任何给定时间，屏幕上会保留多少条带
+        ribbonCount: 2,
+        // 添加笔划以及色带填充颜色
+        strokeSize: 0,
+        // 通过页面滚动上的因子垂直移动色带
+        parallaxAmount: -0.5,
+        // 随着时间的推移，为每个功能区添加动画效果
+        animateSections: true
+      },
+      ribbonShow: false, //  点击彩带  true显示  false为不显示
+      ribbonAnimationShow: true  // 滑动彩带
+    }]
+
+
+  ],
 }
